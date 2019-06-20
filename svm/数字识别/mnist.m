02959a1d593aaca628da0a8dfa31b2376d19a63b
chengyongfu <212666902@qq.com>
2019-06-20 22:05:36 +0800

    补充

--- svm/数字识别/mnist.m
+++ svm/数字识别/mnist.m
@@ -1,14 +1,14 @@
 train_mnist= importdata('mnist_train.csv');
 train=train_mnist.data;
-x_train=train(1:60,2:end);
-y_train=train(1:60,1);
+x_train=train(1:600,2:end);
+y_train=train(1:600,1);
 
 
 
 test_mnist= importdata('mnist_test.csv');
 test=test_mnist.data;
-x_test=test(1:10,2:end);
-y_test=test(1:10,1);
+x_test=test(1:100,2:end);
+y_test=test(1:100,1);
 
 
 options='-t 0 -q';
